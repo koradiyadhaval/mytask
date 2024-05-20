80837872f0f006e6dad14305b85ee1d18085ee90
Dhaval K <dhavalrndev@outlook.com>
2024-05-20 18:21:48 +0530

    update app .tsx

--- App.tsx
+++ App.tsx
@@ -4,18 +4,19 @@
  *
  * @format
  */
-import * as React from 'react';
-// import {render} from 'react-dom';
-import {Provider} from 'react-redux';
-// import React from 'react';
-// import type {PropsWithChildren} from 'react';
-import {ApplicationWrapper} from './src/Components/ApplicationWrapper';
-import {store} from './src/store';
+import * as React from "react";
+import { Provider } from "react-redux";
+import { ApplicationWrapper } from "./src/Components/ApplicationWrapper";
+import { store } from "./src/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./src/store";
 
 function App(): React.JSX.Element {
   return (
     <Provider store={store}>
-      <ApplicationWrapper />
+      <PersistGate persistor={persistor} loading={null}>
+        <ApplicationWrapper />
+      </PersistGate>
     </Provider>
   );
 }
